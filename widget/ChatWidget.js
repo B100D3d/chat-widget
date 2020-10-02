13e5ee6aec7c0c239f1a7270fb3bc7ff92891f2b
B100D3d <alexblooded@gmail.com>
2020-10-02 19:57:10 +0300

    add: ChatWidget class with start logic and styles

---
+++ widget/ChatWidget.js
@@ -0,0 +1,39 @@
+import styles from "./styles/chat.sass"
+
+export default class ChatWidget {
+    rootNode = null
+    ws = null
+
+    constructor() {
+        this.init()
+    }
+
+    init() {
+        this.renderRoot()
+        this.wsConnect()
+    }
+
+    wsConnect() {
+        this.ws = new WebSocket("ws://localhost:3000")
+
+        this.ws.onopen = (e) => {
+            console.log(e)
+        }
+
+        this.ws.onclose = (e) => {
+            console.log(e)
+        }
+
+        this.ws.onmessage = (e) => {
+            console.log(JSON.parse(e.data))
+        }
+    }
+
+    renderRoot() {
+        if (!this.rootNode) {
+            this.rootNode = document.createElement("div")
+            this.rootNode.classList.add(styles.chat)
+            document.body.appendChild(this.rootNode)
+        }
+    }
+}
